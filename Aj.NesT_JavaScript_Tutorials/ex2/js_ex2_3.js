93892c22bc91238d298d8aef4ba0e56472fdee49
Aj. NesT the Series <46470869+ajnesttheseries@users.noreply.github.com>
2021-10-03 13:13:58 +0700

    Add files via upload

---
+++ Aj.NesT_JavaScript_Tutorials/ex2/js_ex2_3.js
@@ -0,0 +1,3 @@
+function myFunctionEx2_3() {
+    document.getElementById("ex2-3").innerHTML = "[External File] I'm great 😃.";
+  }
---
+++ Aj.NesT_JavaScript_Tutorials/ex4/js_ex4_1.js
@@ -0,0 +1,7 @@
+//JavaScript by Aj.NesT the Series 
+//Ex4.1: JavaScript Statements and Assignments
+var num1, num2, result; //Declare 3 Variables var-> use all
+num1 = 3;   //Assign num1 -> Data Type: Number
+num2 = 6;   //Assign num2 -> Data Type: Number
+result = num1 + num2; //Assign result -> Calculation 
+document.getElementById("ex4-1").innerHTML = "The value of result is " + (num1 + num2) +" or "+ result + ".";
