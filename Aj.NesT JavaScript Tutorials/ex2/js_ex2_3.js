502268e74d08ab5a48a34cec10778a3cf96fb274
Aj. NesT the Series <46470869+ajnesttheseries@users.noreply.github.com>
2021-10-03 13:13:29 +0700

    Delete Aj.NesT JavaScript Tutorials directory

--- Aj.NesT JavaScript Tutorials/ex2/js_ex2_3.js
+++
@@ -1,3 +0,0 @@
-function myFunctionEx2_3() {
-    document.getElementById("ex2-3").innerHTML = "[External File] I'm great 😃.";
-  }
--- Aj.NesT JavaScript Tutorials/ex4/js_ex4_2.js
+++
@@ -1,5 +0,0 @@
-//JavaScript by Aj.NesT the Series 
-//Ex4.2: JavaScript Numbers and Strings
-document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
-document.getElementById("ex4-2-2").innerHTML = "Aj.NesT"; //String
-document.getElementById("ex4-2-3").innerHTML = "Aj.NesT score: " + 93.69; //Number + String
